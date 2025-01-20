e87b12dcfc7fe75ea3cddffa62e5b6ba21c5c80d
mohanraj-exe <smohan1029@gmail.com>
2025-01-20 15:31:37 +0530

    continue_break

--- index.php
+++ index.php
@@ -1,30 +1,21 @@
 <?php 
 
-// conditional statements
-$price = 20;
-if($price > 20){
-    echo 'the condition is met';
-} else{
-   echo 'the condition is not met';
-}
-
-// conditional statements inside the for loop
 $products = [
-    ['name' => 'soap', 'price' => 2],
-    ['name' => 'comb', 'price' => 20],
-    ['name' => 'socks', 'price' => 30],
-    ['name' => 'towel', 'price' => 40],
+    ['name' => 'soap', 'price' => 20],
+    ['name' => 'towel', 'price' => 30],
+    ['name' => 'pen', 'price' => 40],
+    ['name' => 'paper', 'price' => 50]
 ];
 
 foreach($products as $product){
-    if($product['price'] < 15 && $product['price'] >= 2){
-        echo $product['name'] . '<br />';
+    if($product['name'] == 'paper'){
+        break;
     }
-
-    if($product['price'] > 30 || $product['price'] < 20){
-        echo $product['name'] . '<br />';
+    if($product['price'] >= 40){
+        continue;
     }
-}
+    echo $product['name'] . "<br />";
+};
 
 ?>
 
@@ -33,17 +24,9 @@ foreach($products as $product){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conditional statement</title>
+    <title>Continue and break</title>
 </head>
 <body>
-    <div>
-        <ul>
-        <?php foreach($products as $product) { ?>
-            <?php if($product['price'] >= 2) { ?>
-                <li><?php echo $product['name'] ?></li>
-            <?php } ?>
-        <?php }?>
-        </ul>
-    </div>
+    
 </body>
 </html> 
