b142505c858a394b459564d64ebf097d6912a4e0
mohanraj-exe <smohan1029@gmail.com>
2025-01-20 17:53:23 +0530

    variable_n_global_scope

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?php 
 
-$array1 = [
-    ['name' => 'mario', 'age' => 20],
-    ['name' => 'yoshi', 'age' => 30]
-];
+// variable scope - block scope
+function myFunc(){
+    $price = 10;
+    echo $price;
+}
 
-echo $array1[0]['name'];
+myFunc();
+echo $price;
 
-// built-in functions
-$color = 'crimson';
-echo strtoupper($color);
-
-// custom function
-// #1
-function sayHello($name = 'shaun'){
-    echo "good morning $name";
+// Global scope
+$name = 'mario';
+function sayHello(){
+    global $name;
+    $name = 'shaun'
+;    echo "hello $name ";
 };
 
 sayHello();
+echo $name;
 
-// #2 - passing associative array as an argument 
-function formatProduct($product){
-return "{$product[1]['name']} costs {$product[1]['price']}";
+// Over-riding the global variable by writing '&' prefix to an argument
+$name = 'mario';
+function sayBye(&$name){
+    $name = 'shaun'
+;    echo "bye $name ";
 };
 
-$formatted = formatProduct([['name' => 'gold star', 'price' => 20], ['name' => 'silver star', 'price' => 10]]);
-echo $formatted;
+sayBye($name);
+echo $name;
+
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +37,7 @@ echo $formatted;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Functions</title>
+    <title>Variable scope</title>
 </head>
 <body>
 
