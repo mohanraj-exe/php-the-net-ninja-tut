c35b8cef983b702b0de78005699d5d86a935ef8f
mohanraj-exe <smohan1029@gmail.com>
2025-01-20 17:20:27 +0530

    functions

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php 
 
-$products = [
-    ['name' => 'soap', 'price' => 20],
-    ['name' => 'towel', 'price' => 30],
-    ['name' => 'pen', 'price' => 40],
-    ['name' => 'paper', 'price' => 50]
+$array1 = [
+    ['name' => 'mario', 'age' => 20],
+    ['name' => 'yoshi', 'age' => 30]
 ];
 
-foreach($products as $product){
-    if($product['name'] == 'paper'){
-        break;
-    }
-    if($product['price'] >= 40){
-        continue;
-    }
-    echo $product['name'] . "<br />";
+echo $array1[0]['name'];
+
+// built-in functions
+$color = 'crimson';
+echo strtoupper($color);
+
+// custom function
+// #1
+function sayHello($name = 'shaun'){
+    echo "good morning $name";
 };
 
+sayHello();
+
+// #2 - passing associative array as an argument 
+function formatProduct($product){
+return "{$product[1]['name']} costs {$product[1]['price']}";
+};
+
+$formatted = formatProduct([['name' => 'gold star', 'price' => 20], ['name' => 'silver star', 'price' => 10]]);
+echo $formatted;
 ?>
 
 <!DOCTYPE html>
@@ -24,9 +33,9 @@ foreach($products as $product){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Continue and break</title>
+    <title>Functions</title>
 </head>
 <body>
-    
+
 </body>
 </html> 
